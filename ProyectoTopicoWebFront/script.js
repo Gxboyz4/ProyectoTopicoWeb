f4c44b35b39090a1bdfcbc28411414f0f704c5af
Gabriel Mancinas <125517545+Gxboyz4@users.noreply.github.com>
2024-11-09 19:33:31 -0700

    Login y register component
    css, html y js (eventos) del login y register component

--- ProyectoTopicoWebFront/script.js
+++ ProyectoTopicoWebFront/script.js
@@ -7,12 +7,18 @@ import { CommentComponent } from "./src/components/comment/comment.component.js"
 import { PrincipalComponent } from "./src/components/principal/principal.component.js";
 import { RightbarComponent } from "./src/components/rightbar/rightbar.component.js";
 import { ComunidadPage } from "./src/pages/community/community.page.js";
+import { LoginComponent } from "./src/components/login/login.component.js";
+import { LoginPage } from "./src/pages/login/login.page.js";
+import { RegisterComponent } from "./src/components/register/register.component.js";
+import { RegisterPage } from "./src/pages/register/register.page.js";
 
 document.addEventListener('DOMContentLoaded', () => {
     //Configuracion de Rutas
     page('/', () => showContent('app-home'));
     page('/comunidad', () => showContent('app-comunidad'));
     page('*', () => showContent('app-home'));
+    //Para entrar aquí tienes que poner en el navegador: http://localhost:5000/login
+    page('/login', () => showContent('app-login'));
     //Inicializar nuestro router
     page();
 });
@@ -30,7 +36,10 @@ window.customElements.define('app-posts', PostsComponent);
 window.customElements.define('app-post', PostComponent);
 window.customElements.define('app-comment', CommentComponent);
 window.customElements.define('app-principal', PrincipalComponent);
-
+window.customElements.define('app-logincomp', LoginComponent);
+window.customElements.define('app-registercomp', RegisterComponent);
 //pages
 window.customElements.define('app-home', HomePage);
 window.customElements.define('app-comunidad', ComunidadPage);
+window.customElements.define('app-login', LoginPage);
+window.customElements.define('app-register', RegisterPage);
---
+++ ProyectoTopicoWebFront/src/components/login/login.component.js
@@ -0,0 +1,65 @@
+export class LoginComponent extends HTMLElement {
+    constructor() {
+        super();
+        this.showPassword = false; 
+    }
+
+    connectedCallback() {
+        const shadow = this.attachShadow({ mode: 'open' });
+        this.#addStyles(shadow);
+        this.#render(shadow);
+        this.#addEventListeners(shadow);
+    }
+
+    #render(shadow) {
+        shadow.innerHTML += `
+            <div class="login-container">
+                <div class="login-box">
+                 <div class="close-button" id="close-login">X</div>
+                    <img src="../src/assets/images/logo.png" alt="Logo" class="logo-image" />
+                    <h2 class="login-title">Iniciar Sesión</h2>
+                    
+                    <div class="input-group">
+                        <div class="input-wrapper">
+                            <input id="email" type="text" placeholder="Correo" class="input-field" />
+                        </div>
+                    </div>
+                    
+                    <div class="input-group">
+                        <div class="input-wrapper">
+                            <input id="password" type="password" placeholder="Contraseña" class="password-input" />
+                            <img src="/src/assets/icons/ShowPassWordIcon.svg" alt="Mostrar contraseña" class="toggle-password" />
+                        </div>
+                    </div>
+                    
+                    <button class="login-button">Entrar</button>
+                </div>
+            </div>
+        `;
+    }
+
+    #addStyles(shadow) {
+        let link = document.createElement("link");
+        link.setAttribute("rel", "stylesheet");
+        link.setAttribute("href", "../src/components/login/login.component.css");
+        shadow.appendChild(link);
+    }
+
+    #addEventListeners(shadow) {
+        const passwordInput = shadow.querySelector(".password-input");
+        const togglePassword = shadow.querySelector(".toggle-password");
+
+        togglePassword.addEventListener("click", () => {
+            this.showPassword = !this.showPassword;
+            passwordInput.type = this.showPassword ? "text" : "password";
+            togglePassword.src = this.showPassword 
+                ? "/src/assets/icons/HidePasswordIcon.svg"
+                : "/src/assets/icons/ShowPassWordIcon.svg";
+        });
+
+        const closeButton = shadow.querySelector("#close-login");
+        closeButton.addEventListener("click", () => {
+        window.location.href = '/'; //URL
+    });
+    }
+}
