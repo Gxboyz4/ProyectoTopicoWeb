4d07fcbca55206477202f9c8a46da9754e62d27b
Gabriel Mancinas <125517545+Gxboyz4@users.noreply.github.com>
2024-11-10 22:50:15 -0700

    Servicio de comunidad hecho
    Servicio de comunidad hecho

--- ProyectoTopicoWebFront/src/services/comunidad.service.js
+++ ProyectoTopicoWebFront/src/services/comunidad.service.js
@@ -1,9 +1,10 @@
-
-
 import { comunidadData } from '../data/comunidadData.js';
-
 export class ComunidadService {
+    #API_URL = 'http://localhost:3000/';
+    #URL_COMUNIDADES = 'api/comunidades/'; 
+    /*Los primeros métodos son para datos de prueba */
     static getComunidades(){
+        
         // HACER PETICION A LA API
         let allComunidades = [];
         comunidadData.forEach(comunidad => {
@@ -12,7 +13,6 @@ export class ComunidadService {
         return allComunidades;
     }
 
-    
     static getComunidadesRecomendadas(cantidad = 3) {
         let comunidadesRandom = comunidadData.sort(() => 0.5 - Math.random());
         return comunidadesRandom.slice(0, cantidad);
@@ -22,6 +22,36 @@ export class ComunidadService {
         // HACER PETICION A LA API
         return comunidadData.find(comunidad => comunidad.id === Number(comunidadId));
     }
+
+    /*Métodos del servicio */
+    static crearComunidad(comunidad, token) {
+        return fetch(`${this.API_URL}${this.URL_COMUNIDADES}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `${token}`
+            },
+            body: JSON.stringify(comunidad)
+        }).then(response => response.json())
+          .then(data => data);
+    }
+
+    static obtenerComunidadPorId(comunidadId) {
+        return fetch(`${this.API_URL}${this.URL_COMUNIDADES}${comunidadId}`, {
+            method: 'GET'
+        }).then(response => response.json())
+          .then(data => data);
+    }
+
+    static obtenerComunidadesFiltro(filtro) {
+        return fetch(`${this.API_URL}${this.URL_COMUNIDADES}query?filtro=${filtro}`, {
+            method: 'GET'
+        }).then(response => response.json())
+          .then(data => data);
+    }
+
+
+
 
 
 }
