cb0c0a1e883ddb30cb22373928884dce49a785ac
MarioZDR <125427714+MarioZDR@users.noreply.github.com>
2024-11-10 22:15:35 -0700

    Usuario Service

--- ProyectoTopicoWebFront/src/services/usuario.service.js
+++ ProyectoTopicoWebFront/src/services/usuario.service.js
@@ -2,6 +2,8 @@
 
 import {usuariosData} from '../data/usuarioData.js';
 
+const API_URL = 'http://localhost:3000/';
+
 export class UsuarioService {
     static getUsuarios(){
         // HACER PETICION A LA API
@@ -15,5 +17,37 @@ export class UsuarioService {
     static getUsuarioById(usuarioId) {
         const usuario = usuariosData.find(usuario => usuario.id === Number(usuarioId));
         return usuario;
+    }
+
+    static registrarUsuario(usuario){
+        return fetch(`${API_URL}/api/usuarios`, {
+            method: 'POST',
+            body: JSON.stringify(usuario),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(response => response.json())
+        .then(data => data);
+    }
+
+    static iniciarSesion(usuario){
+        return fetch(`${API_URL}/api/usuarios/iniciarSesion`, {
+            method: 'POST',
+            body: JSON.stringify(usuario),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(response => response.json())
+        .then(data => data);
+    }
+
+    static getLikes(usuarioId, token){
+        return fetch(`${API_URL}/api/usuarios/${usuarioId}`, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        }).then(response => response.json())
+        .then(data => data);
     }
 }
